7a429d1cf567f4c8266dcb8d1fcdccd188c02755
Fay <fdraidi@hotmail.com>
2023-10-22 17:39:10 +0200

    avant le test d'unicité

--- gr_nor.c
+++ gr_nor.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    int tableau[] = {10, 20, 30, 40, 50, 60}; // Déclaration et initialisation du tableau
+    int tableau[] = {1,2,1,3,1,4,1,5,6,6}; // Déclaration et initialisation du tableau
 
+ // Calculer le nombre d'éléments dans le tableau
+    int n = sizeof(tableau) / sizeof(tableau[0]);
+
+    // Afficher le nombre d'éléments
+    printf("Nombre d'éléments dans le tableau : %d\n", n);
     // Pour afficher les éléments du tableau :
     printf("Contenu du tableau :\n");
-    for (int i = 0; i < 6; i++) {
+    for (int i = 0; i < n; i++) {
         printf("Élément %d : %d\n", i, tableau[i]);
     }
 
+
+ 
     return 0;
 }
