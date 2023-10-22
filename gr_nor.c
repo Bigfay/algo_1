ec94545c73f84b422b318d80944f267882f7c74c
Fay <fdraidi@hotmail.com>
2023-10-22 17:48:16 +0200

    affiche les éléments uniques

--- gr_nor.c
+++ gr_nor.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdbool.h>
+
 
 int main() {
     int tableau[] = {1,2,1,3,1,4,1,5,6,6}; // Déclaration et initialisation du tableau
@@ -13,8 +15,32 @@ int main() {
     for (int i = 0; i < n; i++) {
         printf("Élément %d : %d\n", i, tableau[i]);
     }
+// Déclaration et initialisation d'un tableau dynamique pour stocker les éléments uniques
+    int tableau_elements_uniques[n];
+    int taille_elements_uniques = 0;
 
+    // Parcourir le tableau d'origine
+    for (int i = 0; i < n; i++) {
+        bool est_unique = true;
 
+        // Vérifier si l'élément existe déjà dans le tableau des éléments uniques
+        for (int j = 0; j < taille_elements_uniques; j++) {
+            if (tableau[i] == tableau_elements_uniques[j]) {
+                est_unique = false;
+                break;
+            }
+        }
+
+        // Si l'élément est unique, l'ajouter au tableau des éléments uniques
+        if (est_unique) {
+            tableau_elements_uniques[taille_elements_uniques++] = tableau[i];
+        }
+    }
+  printf("Éléments uniques :\n");
+    for (int i = 0; i < taille_elements_uniques; i++) {
+        printf("%d ", tableau_elements_uniques[i]);
+    }
+    printf("\n");
  
     return 0;
 }
