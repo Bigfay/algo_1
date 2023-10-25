9026b3556d01bb8b8920d049827ce568b4db83e3
Fay <fdraidi@hotmail.com>
2023-10-25 20:47:07 +0200

    première version appurée connexe.c

--- connexe.c
+++ connexe.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
-// VERSION QUI MARCHE
+// VERSION NETTOYEE
 void couples_binaires(int a,int nombre[], int counter, int fer,int wtf[a]) {
     //int wtf[a];
     for (int k = 0;k<a;k++){
@@ -17,12 +17,13 @@ void couples_binaires(int a,int nombre[], int counter, int fer,int wtf[a]) {
 
 
 int main() {
-    int tableau[] = {1,2,2,3,5,5,1,4,4,6}; // Déclaration et initialisation du tableau
+    int tableau[] = {1,2,2,6,3,5,4,5,1,6,3,4,2,5}; // Déclaration et initialisation du tableau
 
  // Calculer le nombre d'éléments dans le tableau
     int n = sizeof(tableau) / sizeof(tableau[0]);
     int fin=0;
-
+    int somme = 0;
+    bool est_connexe;
     // Afficher le nombre d'éléments
     printf("Nombre d'éléments dans le tableau : %d\n", n);
     // Pour afficher les éléments du tableau :
@@ -52,10 +53,8 @@ int main() {
             tableau_elements_uniques[taille_elements_uniques++] = tableau[i];
         }
     }
-    int wtu[taille_elements_uniques];
     int wt_0[taille_elements_uniques];
     int wt_1[taille_elements_uniques];
-
 
   printf("\n");
   printf("Éléments uniques: ");
@@ -65,36 +64,13 @@ int main() {
     printf("\n");
     int r=0;
     // Les positions paires représentent le début d'une arête et impaires la fin de celle-ci
-    for (int i=0;i<taille_elements_uniques;i++){
-        for (int j=0;j<n;j++){
-            if (tableau_elements_uniques[i]==tableau[j]){
-                printf("%d      %d ", j,tableau_elements_uniques[i]);
-                if (j % 2 == 0){
-                    printf("pair ");
-                    printf(" %d ", tableau[j+1]);
-                    r = tableau[j+1];
-                }
-                    else {
-                    printf("impair ");
-                    printf(" %d ", tableau[j-1]);
-                    r = tableau[j-1];
-                    }
-                printf("couple %d %d      ", tableau[j],r); 
-
-                    couples_binaires(taille_elements_uniques,tableau, j,r,wt_0);
-                    printf("\n");               
-                }
-//printf("\n");
-            }
-        }  
-//    printf("\n");
+    
   //parcourir le tableau initiale avec les pairs et les impairs et comparer le courant avec le suivant
 
     couples_binaires(taille_elements_uniques,tableau, 0,tableau[1],wt_1);
   // dupliquer le wt_1 dans le wt_0
     for(int i = 0; i < taille_elements_uniques; ++i) {
         wt_0[i]=wt_1[i];
-        //printf("wt_0  wt_1%d %d  ", wt_0[i],wt_1[i]); 
     }
     printf("\n");
     //wt_0=couples_binaires(taille_elements_uniques,tableau, 0,tableau[1],wt_1);
@@ -102,9 +78,10 @@ int main() {
     //wt_0 = wt_1 =1 alors fusionner les uns
     int merge =0;
   for (int z=2; z < n;z=z+2){
-  //for (int z=2; z < n-2;z=z+2){
-    //printf("premier couple %d %d  ", tableau[z],tableau[z+1]); 
     couples_binaires(taille_elements_uniques,tableau, z,tableau[z+1],wt_1);
+//comparaison entre wt_0 et wt_1 pour voir s'il y a des positions communes contenant un 1
+// si c'est le cas on fusionne les positions en 1
+// si non le nouveau wt_1 devient et écrase le wt_0 et devient la nouvelle référence
     for (int j=0;j<taille_elements_uniques;j++){
         if (wt_0[j]==wt_1[j] && wt_0[j] ==1){
             
@@ -112,10 +89,8 @@ int main() {
             merge=1;
         }
         }
-    
+    //fusion entre wt_0 et wt_1
     if (merge ==1) {
-        //printf("\n"); 
-        //printf("wt_0  :  ");
         for (int k=0; k < taille_elements_uniques;k++){
             if (wt_1[k]==1){
                     wt_0[k]=1;
@@ -129,12 +104,11 @@ int main() {
             }
         }
     printf("      ");
+    //affichage en notation binaire
     for (int i = 0;i<taille_elements_uniques;i++){
         printf(" %d ", wt_0[i]); 
     }
     printf("\n"); 
-
-    int somme = 0;
 
     for (int i = 0; i < taille_elements_uniques; ++i) {
         somme += wt_0[i];
@@ -142,6 +116,7 @@ int main() {
     }
     if (somme==taille_elements_uniques){
         printf("ce graphe est connexe \n");
+        est_connexe = true;
         break;
     }
     else {
@@ -151,16 +126,14 @@ int main() {
              fin=fin+1;
              if (fin==3) {
              printf("ce graphe n'est pas connexe \n");
+             est_connexe = false;
              break;
              }
          };
      }
-
     }
-    //printf("    deuxième couple    %d %d  ", tableau[z+2],tableau[z+3]); 
-    //couples_binaires(taille_elements_uniques,tableau, z+2,tableau[z+3],wt_1);
      printf("\n"); 
     
-    return 0;
+    return est_connexe;
     }
 
