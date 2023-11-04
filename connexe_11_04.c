8173157e854d227579726e4f1dbc405d8a755436
Fay <fdraidi@hotmail.com>
2023-11-04 17:04:51 +0100

    version avec binaire = max valeurs uniques

--- connexe_11_04.c
+++ connexe_11_04.c
@@ -20,9 +20,9 @@ void couples_binaires(int a,int nombre[], int counter, int fer,int wtf[a]) {
 
 
 int main() {
-    //int tableau[] = {1, 2, 3, 2, 4, 3, 4, 5, 5, 9}; // Déclaration et initialisation du tableau - connexe
+    int tableau[] = {1, 2, 3, 2, 4, 3, 4, 5, 5, 9}; // Déclaration et initialisation du tableau
     //int tableau[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // Déclaration et initialisation du tableau
-    int tableau[] = {16,16,7, 8, 11, 12, 3, 4, 9, 10, 5, 6, 13, 14, 15, 1, 16,12,12, 13, 8, 9, 10, 11, 4, 5, 6, 7, 1, 2, 14, 15, 2, 3,15,20}; // Déclaration et initialisation du tableau
+    //int tableau[] = {16,16,7, 8, 11, 12, 3, 4, 9, 10, 5, 6, 13, 14, 15, 1, 16,12,12, 13, 8, 9, 10, 11, 4, 5, 6, 7, 1, 2, 14, 15, 2, 3,15,20}; // Déclaration et initialisation du tableau
 
 
  // Calculer le nombre d'éléments dans le tableau
@@ -43,7 +43,7 @@ int main() {
 // Déclaration et initialisation d'un tableau dynamique pour stocker les éléments uniques
     int tableau_elements_uniques[n];
     int taille_elements_uniques = 0;
-    int max=0;
+
     // Parcourir le tableau d'origine
     for (int i = 0; i < n; i++) {
         bool est_unique = true;
@@ -61,22 +61,14 @@ int main() {
             tableau_elements_uniques[taille_elements_uniques++] = tableau[i];
         }
     }
-    for (int i=0; i< taille_elements_uniques;i++){
-        if (tableau_elements_uniques[i] >= max){
-            max=tableau_elements_uniques[i];
-        }
-    }
-    /* int wt_0[taille_elements_uniques];
-    int wt_1[taille_elements_uniques]; */
-
-    int wt_0[max];
-    int wt_1[max];
+    int wt_0[taille_elements_uniques];
+    int wt_1[taille_elements_uniques];
 
 
   printf("\n");
   printf("nombre d'éléments uniques : %d \n", taille_elements_uniques);
   int check_taille_elements_uniques= taille_elements_uniques;
-  //taille_elements_uniques=9;
+  taille_elements_uniques=9;
 
   printf("Éléments uniques: ");
     for (int i = 0; i < taille_elements_uniques; i++) {
@@ -88,9 +80,9 @@ int main() {
     
   //parcourir le tableau initiale avec les pairs et les impairs et comparer le courant avec le suivant
 
-    couples_binaires(max,tableau, 0,tableau[1],wt_1);
+    couples_binaires(taille_elements_uniques,tableau, 0,tableau[1],wt_1);
   // dupliquer le wt_1 dans le wt_0
-    for(int i = 0; i < max; ++i) {
+    for(int i = 0; i < taille_elements_uniques; ++i) {
         wt_0[i]=wt_1[i];
     }
     printf("\n");
@@ -99,20 +91,20 @@ int main() {
     //wt_0 = wt_1 =1 alors fusionner les uns
     int merge =0;
   for (int z=2; z < n;z=z+2){
-    couples_binaires(max,tableau, z,tableau[z+1],wt_1);
+    couples_binaires(taille_elements_uniques,tableau, z,tableau[z+1],wt_1);
 //comparaison entre wt_0 et wt_1 pour voir s'il y a des positions communes contenant un 1
 // si c'est le cas on fusionne les positions en 1
 // si non le nouveau wt_1 devient et écrase le wt_0 et devient la nouvelle référence
-    for (int j=0;j<max;j++){
+    for (int j=0;j<taille_elements_uniques;j++){
         if (wt_0[j]==wt_1[j] && wt_0[j] ==1){
             
-            j = max;
+            j = taille_elements_uniques;
             merge=1;
         }
         }
     //fusion entre wt_0 et wt_1
     if (merge ==1) {
-        for (int k=0; k < max;k++){
+        for (int k=0; k < taille_elements_uniques;k++){
             if (wt_1[k]==1){
                     wt_0[k]=1;
             }
@@ -121,17 +113,17 @@ int main() {
     }
     printf("      ");
     //affichage en notation binaire
-    for (int i = 0;i<max;i++){
+    for (int i = 0;i<taille_elements_uniques;i++){
         printf(" %d ", wt_0[i]); 
     }
     printf("\n"); 
     somme=0;
-    for (int i = 0; i < max; ++i) {
+    for (int i = 0; i < taille_elements_uniques; ++i) {
         somme += wt_0[i];
         //printf("\n somme : %d\n", somme);
         merge=0;
     }
-    if (somme==taille_elements_uniques){
+    if (somme==check_taille_elements_uniques){
         printf("ce graphe est connexe \n");
         est_connexe = true;
         break;
