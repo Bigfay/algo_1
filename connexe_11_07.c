e38fc8c1b8d8af9304f49ed4b09cee1e328f27a8
Fay <fdraidi@hotmail.com>
2023-11-07 19:15:23 +0100

    moins d'arguments pour la fonction couples_binaires

--- connexe_11_07.c
+++ connexe_11_07.c
@@ -3,9 +3,9 @@
 
 // VERSION NETTOYEE AVEC BUG A CORRIGER
 
-void couples_binaires(int e_uni[],int taille_e_uni,int e_cour, int e_suiv,int wtf[taille_e_uni]) {
+void couples_binaires(int e_uni[],int taille_e_uni,int tableau[], int z, int e_suiv,int wtf[taille_e_uni]) {
     for (int k = 0;k<taille_e_uni;k++){
-                    if (e_uni[k] == e_suiv || e_uni[k] == e_cour){
+                    if (e_uni[k] == e_suiv || e_uni[k] == tableau[z]){
                         wtf[k]=1;
                     }
                         else {
@@ -15,12 +15,13 @@ void couples_binaires(int e_uni[],int taille_e_uni,int e_cour, int e_suiv,int wt
     }
 }
 
+
 int main() {
     //int tableau[] = {2, 15, 3, 2}; // Déclaration et initialisation du tableau - connexe
     //int tableau[] = {19, 2, 3, 2, 4, 3, 4, 5, 5, 90}; // Déclaration et initialisation du tableau - connexe
     //int tableau[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // Déclaration et initialisation du tableau
-    //int tableau[] = {16,16,7, 8, 11, 12, 3, 4, 9, 10, 5, 6, 13, 14, 15, 1, 16,12,12, 13, 8, 9, 10, 11, 4, 5, 6, 7, 1, 2, 14, 15, 2, 3,20,20}; // Déclaration et initialisation du tableau - connexe
-    int tableau[] = {16,16,7, 8, 11, 12, 3, 4, 9, 10, 5, 6, 13, 14, 15, 1, 16,12,12, 13, 8, 9, 10, 11, 4, 5, 6, 7, 1, 2, 14, 15, 2, 3,15,20}; // Déclaration et initialisation du tableau - connexe
+    int tableau[] = {16,16,7, 8, 11, 12, 3, 4, 9, 10, 5, 6, 13, 14, 15, 1, 16,12,12, 13, 8, 9, 10, 11, 4, 5, 6, 7, 1, 2, 14, 15, 2, 3,20,20}; // Déclaration et initialisation du tableau - connexe
+    //int tableau[] = {16,16,7, 8, 11, 12, 3, 4, 9, 10, 5, 6, 13, 14, 15, 1, 16,12,12, 13, 8, 9, 10, 11, 4, 5, 6, 7, 1, 2, 14, 15, 2, 3,15,20}; // Déclaration et initialisation du tableau - connexe
 
 
  // Calculer le nombre d'éléments dans le tableau
@@ -76,7 +77,7 @@ int main() {
     
   //parcourir le tableau initiale avec les pairs et les impairs et comparer le courant avec le suivant
 
-    couples_binaires(tableau_elements_uniques,taille_elements_uniques, tableau[0],tableau[1],wt_1);
+    couples_binaires(tableau_elements_uniques,taille_elements_uniques,tableau, 0,tableau[1],wt_1);
   // dupliquer le wt_1 dans le wt_0
     for(int i = 0; i < taille_elements_uniques; ++i) {
         wt_0[i]=wt_1[i];
@@ -84,7 +85,7 @@ int main() {
     printf("\n");
     int merge =0;
   for (int z=2; z < n;z=z+2){
-    couples_binaires(tableau_elements_uniques,taille_elements_uniques,tableau[z],tableau[z+1],wt_1);
+    couples_binaires(tableau_elements_uniques,taille_elements_uniques,tableau, z,tableau[z+1],wt_1);
 /*comparaison entre wt_0 et wt_1 pour voir s'il y a des positions communes contenant un 1
  si c'est le cas on fusionne les positions en 1
  si non le nouveau wt_1 devient et écrase le wt_0 et devient la nouvelle référence */
